58d98c1e3de6e06f0d964e4b0b9dd2ec62e84112
Bay, Jason <Jason.Bay@adesso.de>
2025-11-26 07:55:07 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SplashCursor from './SplashCursor';
+
+function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <SplashCursor />
+    </div>
+  );
+}
+
+export default App;
