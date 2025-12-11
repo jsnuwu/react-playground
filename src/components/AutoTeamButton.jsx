f511fcc20264395fe7fccb0128d916711b4629f7
Bay, Jason <Jason.Bay@adesso.de>
2025-12-11 12:56:23 +0100

    ki guter status

--- src/components/AutoTeamButton.jsx
+++ src/components/AutoTeamButton.jsx
@@ -4,7 +4,9 @@ import tryExtractJson from "../data/tryExtractJson";
 import "../styles/AutoTeamButton.css";
 import Notification from "./Notification";
 import defaultAvatar from "../assets/profilePictures/default.jpg";
-import AIAvatar from "../assets/chatAvatar/AiAvatar.png";
+import AIAvatar from "../assets/chatIcons/AiAvatar.png";
+import AiLoader from "../assets/chatIcons/AiLoading.gif";
+import AiLoaderNoAnimation from "../assets/chatIcons/AiLoadingNotAnimation.png";
 
 const AutoTeamButton = ({ redTeam, setRedTeam, blueTeam, setBlueTeam }) => {
   const { playerData } = useContext(PlayerContext);
@@ -53,6 +55,7 @@ const AutoTeamButton = ({ redTeam, setRedTeam, blueTeam, setBlueTeam }) => {
 
       const combinedPrompt = `
 Hier sind die Spieler-Daten (nur als Referenz, verwende sie falls relevant):
+Jenny und Mechu sind Frauen.
 ${JSON.stringify(minimal)}
 
 Nutzer fragt:
@@ -191,18 +194,12 @@ Mechu → top / support
             onClick={handleAiInteraction}
             disabled={loading}
           >
-            <svg
-              height="24"
-              width="24"
-              fill="#FFFFFF"
-              viewBox="0 0 24 24"
-              data-name="Layer 1"
-              id="Layer_1"
-              className="sparkle"
-            >
-              {" "}
-              <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>{" "}
-            </svg>
+            <div className="btn-icon">
+              <img
+                src={loading ? AiLoader : AiLoaderNoAnimation}
+                alt="loader-img"
+              />
+            </div>
             <span className="text">{loading ? "Thinking..." : "Generate"}</span>
           </button>
         </div>
